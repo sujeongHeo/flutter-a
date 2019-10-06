12b1796d004c73de149ca4942cdc5dacf98b51f2
sujeongHeo <gjtnwjd40@gmail.com>
2019-10-06 22:22:14 +0900

    Make horizontal item and add this to New tab

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
+          primaryColor: Colors.white
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+//        primarySwatch: Colors.white
       ),
       home: RandomWords(),
 
@@ -37,13 +38,52 @@ class RandomWordState extends State<RandomWords>{
   Widget build(BuildContext context){
     return Scaffold(
         appBar: AppBar(
-            title: Text('Startup Name Generator')
+            title: Text('Startup Name Generator'),
+            actions: <Widget>[
+              IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+              ],
+
         ),
+//            https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2/#5
+
         body: _buildSuggestions(),
     );
 
   }
 
+  
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(    //Add 20 lines from here.. 
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair){
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body:ListView(children: divided),
+          );
+        
+      },
+      ),
+      
+    );
+  }
   Widget _buildSuggestions(){
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
